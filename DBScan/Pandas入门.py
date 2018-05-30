b5669279b7e78dc7652d35d05747642e6cf2fabf
= <=>
2018-05-30 21:48:27 +0800

    aa1

--- DBScan/Pandas入门.py
+++ DBScan/Pandas入门.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 
-filepath = r'D:\WorkSpace\PycharmProjects\TensorFlow\DBScan\hotpic\test.csv'
+filepath = r'D:\WorkSpace\PythonHome\MachineLearningAndDeepLearning\DBScan\hotpic\test.csv'
 
 """
     小区名   房价均值
@@ -20,53 +20,74 @@ filepath = r'D:\WorkSpace\PycharmProjects\TensorFlow\DBScan\hotpic\test.csv'
 data = pd.read_csv(filepath)
 print(data)
 
-"""
-访问数据
-"""
-# 按列
-print(data['房价均值']) # 返回指定列数据+属性
-print(data['房价均值'][0]) # 返回指定cell数据
+# series 和 DataFrame 是 Pandas中最常用的两个对象
 
-# panda 怎么返回正行数据?
+print(data.shape)
 
+# index保存行索引，columns保存列索引
+print(data.columns)
 
-"""
-查看数据
-"""
-print(data.head(2)) # 返回前两条数据
+# 某行所有列
+print(data.ix[0, :])  # #第一行所有数据  ix(index 缩写 )
 
-print(data.tail(2)) #  tail() 参数为空 返回全部 参数为 n 返回倒数n行的所有数据
+# 某几行-所有列
+print(data.ix[[0, 2],])  # 获取第1/3行的数据
 
-"""
-查看数据的index
-"""
+# 所有行-所有列
+print(data.ix[:, :])
 
-print(data.index) # RangeIndex(start=0, stop=3, step=1)
-print(data.values) # [['一品新筑' 15667]
-                     #['丁香大楼' 16000]
-                     #['万兆家园' 5057]]
+# 某列-所有行
+print(data.ix[:, '房价均值'])
+
+# 某几列-某几行
+print(data.ix[[0, 2], ['小区名', '房价均值']])
 
 
-"""
-查看数据概况
-"""
+def pd_init(data):
+	"""
+	访问数据
+	"""
+	# 按列
+	print(data['房价均值'])  # 返回指定列数据+属性
+	print(data['房价均值'][0])  # 返回指定cell数据
 
-print(data.describe())
+	# panda 怎么返回正行数据?
 
-"""
-               房价均值
-count      3.000000
-mean   12241.333333
-std     6224.042604
-min     5057.000000
-25%    10362.000000
-50%    15667.000000
-75%    15833.500000
-max    16000.000000
-"""
+	"""
+	查看数据
+	"""
+	print(data.head(2))  # 返回前两条数据
+
+	print(data.tail(2))  # tail() 参数为空 返回全部 参数为 n 返回倒数n行的所有数据
+
+	"""
+	查看数据的index
+	"""
+
+	print(data.index)  # RangeIndex(start=0, stop=3, step=1)
+	print(data.values)  # [['一品新筑' 15667]
+	# ['丁香大楼' 16000]
+	# ['万兆家园' 5057]]
+
+	"""
+	查看数据概况
+	"""
+
+	print(data.describe())
+
+	"""
+	               房价均值
+	count      3.000000
+	mean   12241.333333
+	std     6224.042604
+	min     5057.000000
+	25%    10362.000000
+	50%    15667.000000
+	75%    15833.500000
+	max    16000.000000
+	"""
 
 # 使用csv包打开,返回的是一个二维列表
 # with open(filepath, 'r', encoding='UTF-8') as f:
 #     reader = csv.reader(f)
 #     print(list(reader))
-
--- DBScan/hotpic/hot.py
+++ DBScan/hotpic/hot.py
@@ -1,28 +1,51 @@
 import csv
 import pandas as pd
 
-filepath = r'D:\WorkSpace\PycharmProjects\TensorFlow\DBScan\hotpic\test.csv'
+from urllib.request import urlopen, quote
+import json
 
-"""
-    小区名   房价均值
-0  一品新筑  15667
-1  丁香大楼  16000
-2  万兆家园   5057
-
-使用panda打开 能保持csv的原始格式,可以分为三部分
-(1) 列名
-(2) 索引
-(3) 数据
-
-"""
+filepath = r'test.csv'
 
 # 使用panda打开 能保持csv的原始格式
 data = pd.read_csv(filepath)
 print(data)
 
 
-# 使用csv包打开,返回的是一个二维列表
-# with open(filepath, 'r', encoding='UTF-8') as f:
-#     reader = csv.reader(f)
-#     print(list(reader))
+def getlnglat(address):
+	"""根据传入地名参数获取经纬度"""
+	url = 'http://api.map.baidu.com/geocoder/v2/'
+	output = 'json'
+	ak = 'Ti4YbLTnbrA32UvN3QFt20fK79ufDmds'  # 浏览器端密钥
+	address = quote(address)
+	uri = url + '?' + 'address=' + address + '&output=' + output + '&ak=' + ak
+	req = urlopen(uri)
+	res = req.read().decode()
+	temp = json.loads(res)
+	lat = temp['result']['location']['lat']
+	lng = temp['result']['location']['lng']
+	return lat, lng
 
+
+file = open(r'经纬度.json', 'w')  # 建立json数据文件
+
+with open(filepath, 'r', encoding = 'UTF-8') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		# print('line = ', line)
+		if reader.line_num == 1:
+			continue
+		cellname = line[0].strip()
+		price = line[1].strip()
+
+		try:
+			lat, lng = getlnglat(cellname)
+			str_temp = '{"lat:"' + str(lat) + ',"lng":' + str(lng) + ',"count":' + str(price) + '},'
+			print('str_temp = ', str_temp)
+			file.write(str_temp)
+			print("文件保存成功！")
+		except:
+			f = open("异常日志.txt", 'a')
+			f.flush()
+			f.close()
+			print('发生异常')
+file.close()
